52d35d1fc99f61ba917719423d0dc3d309eccb05
PragyaKesar <96820528+PragyaKesar@users.noreply.github.com>
2022-01-05 17:39:39 +0530

    Update Fasta36_SeqSearch.py

--- off-target_prediction_pipeline/Fasta36_SeqSearch.py
+++ off-target_prediction_pipeline/Fasta36_SeqSearch.py
@@ -2,6 +2,7 @@ import sys
 import numpy as np
 import pandas as pd
 import os
+#from fastai.imports import *
 from Bio import AlignIO
 from Bio import SeqFeature
 from Bio import SearchIO
@@ -9,34 +10,45 @@ from Bio.SearchIO import FastaIO
 from Bio.SearchIO.FastaIO import FastaM10Parser
 #Input from command line
 Input=sys.argv[1]
-cmd1 = '../bin/fasta36 -n -E 10000 -k 1 -r [1/-1] -f 5 -g 5 /home/titan-4/Downloads/output_files/'
-cmd2=' /home/titan-4/Downloads/ncbi_dataset/ncbi_dataset/data/GCF_000001405.39/Human_genome.fna -m 10 > /home/titan-4/Downloads/output_files/fasta36_hg38-m1o.txt'
+Input1=sys.argv[2]
+Out_Input=sys.argv[3]
+Fasta36_Out = os.path.dirname(os.path.abspath(Input))
+cmd1 = '../bin/fasta36 -n -E 10000 -k 1 -r [1/-1] -f 5 -g 5 '
+cmd2=' '
+cmd3=' -m 10 > '
+cmd4='\\fasta36_hg38-m1o.txt'
 #concatenating all the variable for form a FASTA36 run command
-cmd=cmd1+Input+cmd2
+cmd=cmd1+Input+cmd2+Input1+cmd3+Fasta36_Out+cmd4
+print(cmd)
 #Run in command line
 os.system(cmd)
+cmd5='\\H1.txt'
+cmd6='\\Q1.txt'
+file_out=Fasta36_Out+cmd4
+file_out1=Fasta36_Out+cmd5
+file_out2=Fasta36_Out+cmd6
 #open the FASTA36 result file
-with open("D://PhD_related/model_evaluation/comparison_deepcpf1-crisprDT/other_datasets/HT_2/HEK-plasmid_seq_hg37-m101.txt", "r+") as script:
+with open(file_out, "r+") as script:
     #format alignment results into a csv file
     for b in FastaM10Parser(script, 'fasta-m10'):
         for alignment in b:
             for c in alignment:
                 for d in c:
                     #seperate alignment for hit sequences from result file generated from FASTA36
-                    file = open("D://PhD_related/model_evaluation/comparison_deepcpf1-crisprDT/other_datasets/HT_2/H1.txt", 'a')
+                    file = open(file_out1, 'a')
                     file.write("%s %s %s %s %s" % (d.hit, d.hit_end, d.hit_start, d.hit_strand, c.ident_pct))
                     file.write("\n")
                     #seperate alignment for gRNA sequences from result file generated from FASTA36
-                    file1 = open("D://PhD_related/model_evaluation/comparison_deepcpf1-crisprDT/other_datasets/HT_2/Q1.txt", 'a')
+                    file1 = open(file_out2, 'a')
                     file1.write("%s" % (d.query))
                     file1.write("\n")
 
 #expression to be searched in alignment file
 search1 = 'Description: '
 search2 = "Seq('"
-f0 = open("D://PhD_related/model_evaluation/comparison_deepcpf1-crisprDT/other_datasets/HT_2/H1.txt", "r")
-f1 = open("D://PhD_related/model_evaluation/comparison_deepcpf1-crisprDT/other_datasets/HT_2/Q1.txt", "r")
-f2 = open("D://PhD_related/model_evaluation/comparison_deepcpf1-crisprDT/other_datasets/HT_2/H2.csv", "a")
+f0 = open(file_out1, "r")
+f1 = open(file_out2, "r")
+f2 = open(Out_Input, "a")
 length1=27
 
 list1=[]
@@ -67,27 +79,24 @@ for line2 in f1:
         line2=line2.replace("\n", "")
         list3.append(line2)
 #display all the rows and columns
-pd.set_option("display.max_rows", None, "display.max_columns", None)
+#pd.set_option("display.max_rows", None, "display.max_columns", None)
 #dataframe with gRNA
 Query_df = pd.DataFrame(list3, columns=['gRNA'])
 #dataframe with off-targets, end, start, strand, identity
 Seq_df1 = pd.DataFrame(list2, columns=['Off-target', 'END', 'START', 'Strand', 'Percentage identity'])
+LenE=Seq_df1["END"]
+LenS=Seq_df1["START"]
+LenD=[]
+for i, j in zip(LenE, LenS): 
+    D=int(i)- int(j)
+    LenD.append(D)
+#dataframe with length
+len_df = pd.DataFrame(LenD, columns=['Length'])
 #dataframe with chromosome number
 chr_df = pd.DataFrame(list1, columns=['Chromosome number'])
 #concatenate all the dataframes to form final dataframe
-OT_df = pd.concat([Query_df, Seq_df1, chr_df], axis=1)
+OT_df = pd.concat([Query_df, Seq_df1, len_df, chr_df], axis=1)
 #extract specific column values
-OT=OT_df['Off-target']
-gRNA=OT_df['gRNA']
-n=0
-#iterate over the column values
-for i, j in zip(OT, gRNA):
-    n=n+1
-    #filter sequence with length = 27
-    if(len(i) == length1) and (len(i) == length1):
-        pass
-    else:
-        #drop columns if length of gRNA and  off-target sequence is not equal to 27
-        OT_df.drop(index=n)
+OT_df1=OT_df[OT_df["Length"] == 27]
 #print final dataframe to command prompt screen
-print(OT_df)
+print(OT_df1)
