2e772d3f5850eeee6033d3d110bf1e153ad23db9
PragyaKesar <96820528+PragyaKesar@users.noreply.github.com>
2023-01-02 14:42:09 +0530

    Update AsCpf1_main.py

--- OT_PredPipelines/CombClassifier/AsCpf1_main.py
+++ OT_PredPipelines/CombClassifier/AsCpf1_main.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 import csv
 import sys
 from difflib import SequenceMatcher
-import AsCpf1FeatureExtractor
+from CombFeatureExtractor_AsCpf1 import AsCpf1_AllFeatureExtractor
 from Bio.SeqUtils import MeltingTemp as mt
 from Bio.Seq import Seq
 import pandas as pd
@@ -17,4 +17,18 @@ import sys
 import RNA
 
 #Probability prediction using AdabostClassifier with pre-defined function
-ClassVar=CombFeatureExtractor_AsCpf1.AsCpf1_AllFeatureExtractor..ModelPred()
+AsCpf1FE = AsCpf1_AllFeatureExtractor()
+AsCpf1FE.seq_encode()
+AsCpf1FE.mismatch()
+AsCpf1FE.bulges()
+AsCpf1FE.mismatch_type()
+AsCpf1FE.GC_contentFn()
+AsCpf1FE.DiNuclFreq()
+AsCpf1FE.MononuclCount()
+AsCpf1FE.RepetitiveSeq()
+AsCpf1FE.MinFreeEnergy()
+AsCpf1FE.MeltingTemp()
+AsCpf1FE.PAMEnergy()
+AsCpf1FE.MismatchEnergy()
+AsCpf1FE.DataFrameConstruct()
+AsCpf1FE.ModelPred()
